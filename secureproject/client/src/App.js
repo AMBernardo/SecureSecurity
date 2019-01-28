83fba3136d44b4e7128b81f72a76a688f08f374d
daniel <you@example.com>
2019-01-28 14:07:19 -0800

    Got the 07-mern-example added for backend

--- secureproject/client/src/App.js
+++ secureproject/client/src/App.js
@@ -1,20 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import NavTabs from "./components/NavTabs";
 import Home from "./components/pages/Home/Home";
 import Login from "./components/pages/login/index";
-import Profile from "./components/pages/porfile/index";
-import Contact from "./components/pages/Contact";
+import Homepage from "./components/pages/Homepage/index";
+import Signup from "./components/pages/signup/Signup";
 
 function App() {
   return (
     <Router>
       <div>
-        
         <Route exact path="/" component={Home} />
-        <Route exact path="/login-signup" component={Login} />
-        <Route exact path="/homepage" component={Profile} />
-        <Route path="/contact" component={Contact} />
+        <Route exact path="/login" component={Login} />
+        <Route path="/signup" component={Signup} />
+        <Route exact path="/homepage" component={Homepage} />
       </div>
     </Router>
   );
--- secureproject/client/src/components/pages/signup/Signup.js
+++ secureproject/client/src/components/pages/signup/Signup.js
@@ -20,7 +20,7 @@ function Contact(props) {
       <Link to={`${props.match.url}/learn`} role="button" className="btn btn-link">
         Learn More
       </Link>{" "}
-      <Link to="/contact" role="button" className="btn btn-link">
+      <Link to="/signup" role="button" className="btn btn-link">
         Learn Less
       </Link>
       <Route exact path={`${props.match.url}/learn`} component={Learn} />
